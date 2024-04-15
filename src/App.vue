5e329f629ee652bc92b0f5e5c4aa0e4ff8780954
MatteD04 <matteo.dantonio04@gmail.com>
2024-04-15 18:19:38 +0200

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader
+  }
+}
 </script>
 
 <template>
+  <header>
+    <AppHeader></AppHeader>
+  </header>
 </template>
 
 <style lang="scss">
+@use './style/generic';
+
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <div class="container header-yugi">
+        <h1>YU-GI-OH API</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.header-yugi{
+    padding: 15px 0;
+}
+</style>
